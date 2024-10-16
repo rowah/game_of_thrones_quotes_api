876d89ab8c14535939cc593a31c0e20df28b5135
Rowa <jrowah@gmail.com>
2024-10-16 16:27:31 +0300

    add httpoison and json deps

--- mix.exs
+++ mix.exs
@@ -58,7 +58,9 @@ defmodule GotQuotesApi.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:httpoison, "~> 2.2"},
+      {:json, "~> 1.4"}
     ]
   end
 
