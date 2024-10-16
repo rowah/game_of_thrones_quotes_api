108c3a9f053048a86219e9f172d607aec766a3ce
Rowa <jrowah@gmail.com>
2024-10-16 16:55:22 +0300

    seed db with random quotes from a secondary api

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,39 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+alias GotQuotesApi.Quotes
+require Logger
+
+defmodule GOTQuotesSeeder do
+  # Fetch 100 random quotes every time I run the seeds file
+  @api_url "https://api.gameofthronesquotes.xyz/v1/random/700"
+
+  def fetch_and_seed_quotes do
+    case HTTPoison.get!(@api_url) do
+      %HTTPoison.Response{
+        status_code: 200,
+        body: body
+      } ->
+        {:ok, quotes} = Jason.decode(body)
+        save_quotes(quotes)
+
+      {%HTTPoison.Response{status_code: status_code}} ->
+        Logger.error("Failed to fetch quotes. Status code: #{status_code}")
+        :error
+
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        Logger.error("Error occurred: #{inspect(reason)}")
+        :error
+    end
+  end
+
+  defp save_quotes(quotes) do
+    Enum.each(quotes, fn %{"character" => character, "sentence" => content} ->
+      Quotes.create_quote(%{character: character, sentence: content})
+    end)
+
+    Logger.info("Quotes successfully inserted into the database.")
+  end
+end
+
+GOTQuotesSeeder.fetch_and_seed_quotes()
